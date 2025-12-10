af7a4a13ff8f321ea87206675df8257960107152
kimyugyum <kyg021003@cau.ac.kr>
2025-12-10 17:33:31 +0900

    fix: styled.d.ts

---
+++ app/styles/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components';
+import { theme } from './theme';
+
+type AppTheme = typeof theme;
+
+declare module 'styled-components' {
+  // DefaultTheme을 프로젝트의 theme 타입으로 확장
+  // (배포 빌드에서 theme.colors 등 타입 인식 오류 방지)
+  export interface DefaultTheme extends AppTheme {}
+}
+
